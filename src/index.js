ad6f54c775fff9bb2ea603ab2e8e216a634c8d75
vinti-roy <vintiray71@gmail.com>
2023-01-09 12:15:43 +0530

    new

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+const route=require("./routes/route")
+
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://vintiray:7091201680@cluster0.ahtxrqr.mongodb.net/test", {
+    useNewUrlParser: true
+})
+
+.then( () => console.log("MongoDb is connected")) 
+.catch ( err => console.log(err) )
+
+app.use('/',route)
+
+app.listen(3000, ()=>{
+    console.log('Express app running on port ' +(3000))
+})
