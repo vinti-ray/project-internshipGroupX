e680dd072b606c847fea089176a5ef3c3e36f27f
vinti-roy <vintiray71@gmail.com>
2023-01-09 20:56:19 +0530

    new

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,35 +1,39 @@
 const mongoose = require('mongoose');
 
-//Name Validation
+//______________________Name Validation____________
 
 const isValid = function (name) {
   const nameRegex = /^[a-zA-Z ]+$/;
   return nameRegex.test(name);
 };
 
-//Email Validation
+//____________________Email Validation_________________
 
 const isValidEmail = function (email) {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(email);
   };
 
+//_____________________mobile number validation_____________________
   const isValidMobile = function (mobile){
     const mobailRegex = /^([+]\d{2})?\d{10}$/;
     return mobailRegex.test(mobile);
 
   };
+
+  //____________________validate url_______________________
   const isValidUrl=function(logoLink){
     const urlPattern = /https?:\/\/(.+\/)+.+(\.(gif|png|jpg|jpeg|webp|svg|psd|bmp|tif|jfif))$/i;
     return urlPattern.test(logoLink)
   }
 
-
+//____________________validate fullName___________________________
   const isValidfullName = function (fullName) {
     const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
     return nameRegex.test(fullName);
   };
 
+//____________________export_______________________________
 
   module.exports = {
     isValid,
